85d2ff8422064ab2c2d8e7fcf6e817eb2352ba0c
nyxara <sebastian.rebaza@outlook.com>
2026-05-04 22:55:57 -0500

    Added Spring Structure

---
+++ src/main/java/com/upc/finexia/FinexiaApplication.java
@@ -0,0 +1,13 @@
+package com.upc.finexia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinexiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinexiaApplication.class, args);
+	}
+
+}
