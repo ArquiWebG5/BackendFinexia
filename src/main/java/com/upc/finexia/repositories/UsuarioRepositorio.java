809970b7979cb9dec39c83a15813d4196520ccd8
nyxara <sebastian.rebaza@outlook.com>
2026-05-14 09:06:13 -0500

    Add: documentation of US for DTO and repository

--- src/main/java/com/upc/finexia/repositories/UsuarioRepositorio.java
+++ src/main/java/com/upc/finexia/repositories/UsuarioRepositorio.java
@@ -4,6 +4,7 @@ import com.upc.finexia.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// Repositorio del perfil de Usuario (HU 03 - perfil, HU 04 - actualizar, HU 05 - eliminar).
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
 }
